60f52f7488d82b976dab399fda585286f4c175c1
FreshWaterLee <ihg7845@naver.com>
2021-08-11 16:55:42 +0900

    08111700

--- Kairess_Example/Finger_Gesture/Gesture_Train.py
+++ Kairess_Example/Finger_Gesture/Gesture_Train.py
@@ -40,3 +40,14 @@ model = Sequential([
 
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['acc'])
 model.summary()
+
+history = model.fit(
+    x_train,
+    y_train,
+    validataion_data = (x_val,y_val),
+    epochs=200,
+    callbacks=[
+        ModelCheckpoint('models/model.h5',monitor='val_acc',verbose=1,save_best_only=True,mode='auto'),
+        ReduceLROnPlateau(monitor='val_acc',factor=0.5,patience=50,verbose=1,mode='auto')
+        ]
+    )
